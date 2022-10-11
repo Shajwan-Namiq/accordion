04f734d3f55e3c2d6e83d551a071b41c6a82341b
Shajwan <shajwannamiqsalh@gmail.com>
2022-10-11 17:05:02 +0300

    adding + btn and - btn for hide and showing info

--- src/Question.js
+++ src/Question.js
@@ -1,14 +1,42 @@
-import React from 'react'
- 
+import React,{useState} from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 const Question = ({title,info}) => {
+  
+  const [showInfo, setShowInfo] = useState(false);
+
+
   return (
-    <div>
-      <div className="border">
-        {title}
-        <button className="bg-orange-600">btn</button>
-        <div>{info}</div>
-      </div>
-    </div>
+    <>
+      <main className="w-4/5 lg:w-3/5 p-5 mx-auto">
+        <section className="shadow row">
+          <div className="border-b tab">
+            <div className="border-l-2 border-transparent relative">
+              <header
+                className="flex justify-between items-center p-5 pl-8 pr-8 cursor-pointer select-none tab-label"
+                
+              >
+                <span className="text-cyan-900 font-bold  text-sm">
+                  {title}
+                </span>
+                <div className="rounded-full border border-grey w-7 h-7 flex items-center justify-center test">
+                  <button
+                    onClick={() => setShowInfo(!showInfo)}
+                    className="h-24 w-24"
+                  >
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                  </button>
+                </div>
+              </header>
+              <div className="tab-content">
+                <div className="pl-8 pr-8 pb-5 text-grey-darkest">
+                  {showInfo && <p>{info}</p>}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
